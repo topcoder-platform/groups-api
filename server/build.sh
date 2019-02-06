9e2af38c17606d0b8632d55cad416b6b5b700eba
Gunasekar-K <Gunasekar.Kesavalu@wipro.com>
2019-02-06 17:09:10 +0530

    Update build.sh
    [skip ci]

--- server/build.sh
+++ server/build.sh
@@ -6,10 +6,10 @@ docker create --name app groups-api:latest
 
 if [ -d node_modules ]
 then
-  mv package-lock.json old-package-lock.json
-  docker cp app:/server/package-lock.json package-lock.json
+  mv server/package-lock.json server/old-package-lock.json
+  docker cp app:/server/package-lock.json server/package-lock.json
   set +eo pipefail
-  UPDATE_CACHE=$(cmp package-lock.json old-package-lock.json)
+  UPDATE_CACHE=$(cmp server/package-lock.json server/old-package-lock.json)
   set -eo pipefail
 else
   UPDATE_CACHE=1
@@ -17,5 +17,5 @@ fi
 
 if [ "$UPDATE_CACHE" == 1 ]
 then
-  docker cp app:/server/node_modules .
+  docker cp app:/server/node_modules ./server/
 fi
