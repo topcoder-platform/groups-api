6df9fc77ea121d6f12656047a00f1ff2be347636
Dushyant Bhalgami <dushyantbhalgami@gmail.com>
2019-02-06 14:47:09 +0530

    change in build.sh and npm modules

--- server/build.sh
+++ server/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -eo pipefail
 UPDATE_CACHE=""
-docker-compose -f docker/docker-compose.yml build leaderboard-api
-docker create --name app leaderboard-api:latest
+docker-compose -f server/docker/docker-compose.yml build groups-api
+docker create --name app groups-api:latest
 
 if [ -d node_modules ]
 then
   mv package-lock.json old-package-lock.json
-  docker cp app:/leaderboard-api/package-lock.json package-lock.json
+  docker cp app:/server/package-lock.json package-lock.json
   set +eo pipefail
   UPDATE_CACHE=$(cmp package-lock.json old-package-lock.json)
   set -eo pipefail
@@ -17,5 +17,5 @@ fi
 
 if [ "$UPDATE_CACHE" == 1 ]
 then
-  docker cp app:/leaderboard-api/node_modules .
+  docker cp app:/groups-api/node_modules .
 fi
