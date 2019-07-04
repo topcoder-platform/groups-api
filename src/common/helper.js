71f024fbf43f77bd3ea4352bf04bb4b27c2c6240
Dushyant Bhalgami <dushyantbhalgami@gmail.com>
2019-07-04 15:13:35 +0530

    fix code

--- src/common/helper.js
+++ src/common/helper.js
@@ -76,16 +76,19 @@ async function ensureExists(tx, model, id) {
   //   throw new errors.NotFoundError(`Not found ${model} of id ${id}`);
   // }
 
-  if (model === 'Group' && (!res || res.records.length === 0 || !res.records[0] || !res.records[0].get(0))) {
+  if (model === 'Group' && res && res.records.length === 0) {
     throw new errors.NotFoundError(`Not found ${model} of id ${id}`);
-  } else if (model === 'User' && (!res || res.records.length === 0 || !res.records[0] || !res.records[0].get(0))) {
-    logger.debug('=====');
+  } else if (model === 'User' && res && res.records.length === 0) {
+    logger.debug('==#==');
     logger.debug('creating user');
-    logger.debug('=====');
+    logger.debug('==#==');
     const user = await tx.run(`CREATE (user:User {id: {id}}) RETURN user`, { id });
     return user.records[0]._fields[0].properties;
   }
-  return res.records[0].get(0).properties;
+  logger.debug('==*==');
+  logger.debug('creating user');
+  logger.debug('==*==');
+  return res.records[0]._fields[0].properties;
 }
 
 /**
