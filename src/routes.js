ba98f04c722c561b6c044994cd85bf089391910a
abhinav-koppula <abhinav.koppula@gmail.com>
2022-10-24 23:31:14 +0530

    [Newman]-Fixes in newman automation

--- src/routes.js
+++ src/routes.js
@@ -11,14 +11,14 @@ module.exports = {
       method: 'searchGroups',
       auth: 'jwt',
       access: [constants.UserRoles.Admin, constants.UserRoles.User],
-      scopes: ['read:groups', 'write:groups', 'all:groups']
+      scopes: ['read:groups', 'write:groups', 'all:groups', 'all:resources']
     },
     post: {
       controller: 'GroupController',
       method: 'createGroup',
       auth: 'jwt',
       access: [constants.UserRoles.Admin],
-      scopes: ['write:groups', 'all:groups']
+      scopes: ['write:groups', 'all:groups', 'all:resources', 'all:resources']
     }
   },
   '/groups/internal/jobs/clean':{
@@ -27,7 +27,7 @@ module.exports = {
       method: 'cleanUpTestData',
       auth: 'jwt',
       access: [constants.UserRoles.Admin],
-      scopes: ['all:groups']
+      scopes: ['all:groups', 'all:resources']
     }
   },
   '/groups/health': {
@@ -42,21 +42,21 @@ module.exports = {
       method: 'getGroup',
       auth: 'jwt',
       access: [constants.UserRoles.Admin, constants.UserRoles.User],
-      scopes: ['read:groups', 'write:groups', 'all:groups']
+      scopes: ['read:groups', 'write:groups', 'all:groups', 'all:resources']
     },
     put: {
       controller: 'GroupController',
       method: 'updateGroup',
       auth: 'jwt',
       access: [constants.UserRoles.Admin],
-      scopes: ['write:groups', 'all:groups']
+      scopes: ['write:groups', 'all:groups', 'all:resources']
     },
     delete: {
       controller: 'GroupController',
       method: 'deleteGroup',
       auth: 'jwt',
       access: [constants.UserRoles.Admin],
-      scopes: ['write:groups', 'all:groups']
+      scopes: ['write:groups', 'all:groups', 'all:resources']
     }
   },
   '/groups/oldId/:oldId': {
@@ -65,7 +65,7 @@ module.exports = {
       method: 'getGroupByOldId',
       auth: 'jwt',
       access: [constants.UserRoles.Admin, constants.UserRoles.User],
-      scopes: ['read:groups', 'write:groups', 'all:groups']
+      scopes: ['read:groups', 'write:groups', 'all:groups', 'all:resources']
     }
   },
   '/groups/:groupId/members': {
@@ -74,21 +74,21 @@ module.exports = {
       method: 'getGroupMembers',
       auth: 'jwt',
       access: [constants.UserRoles.Admin, constants.UserRoles.User],
-      scopes: ['read:groups', 'write:groups', 'all:groups']
+      scopes: ['read:groups', 'write:groups', 'all:groups', 'all:resources']
     },
     post: {
       controller: 'GroupMembershipController',
       method: 'addGroupMember',
       auth: 'jwt',
       access: [constants.UserRoles.Admin, constants.UserRoles.User],
-      scopes: ['write:groups', 'all:groups']
+      scopes: ['write:groups', 'all:groups', 'all:resources']
     },
     delete: {
       controller: 'GroupMembershipController',
       method: 'deleteGroupMember',
       auth: 'jwt',
       access: [constants.UserRoles.Admin, constants.UserRoles.User],
-      scopes: ['write:groups', 'all:groups']
+      scopes: ['write:groups', 'all:groups', 'all:resources']
     }
   },
   '/groups/:groupId/members/:memberId': {
@@ -97,14 +97,14 @@ module.exports = {
       method: 'getGroupMember',
       auth: 'jwt',
       access: [constants.UserRoles.Admin, constants.UserRoles.User],
-      scopes: ['read:groups', 'write:groups', 'all:groups']
+      scopes: ['read:groups', 'write:groups', 'all:groups', 'all:resources']
     },
     delete: {
       controller: 'GroupMembershipController',
       method: 'deleteGroupMember',
       auth: 'jwt',
       access: [constants.UserRoles.Admin, constants.UserRoles.User],
-      scopes: ['write:groups', 'all:groups']
+      scopes: ['write:groups', 'all:groups', 'all:resources']
     }
   },
   '/groups/:groupId/membersCount': {
@@ -125,7 +125,7 @@ module.exports = {
       method: 'createSubGroup',
       auth: 'jwt',
       access: [constants.UserRoles.Admin, constants.UserRoles.User],
-      scopes: ['write:groups', 'all:groups']
+      scopes: ['write:groups', 'all:groups', 'all:resources']
     }
   },
   '/groups/:groupId/subGroup/:subGroupId': {
@@ -134,7 +134,7 @@ module.exports = {
       method: 'deleteSubGroup',
       auth: 'jwt',
       access: [constants.UserRoles.Admin, constants.UserRoles.User],
-      scopes: ['write:groups', 'all:groups']
+      scopes: ['write:groups', 'all:groups', 'all:resources']
     }
   },
   '/groups/memberGroups/groupMembersCount': {
@@ -149,7 +149,7 @@ module.exports = {
       method: 'getMemberGroups',
       auth: 'jwt',
       access: [constants.UserRoles.Admin, constants.UserRoles.User],
-      scopes: ['read:groups']
+      scopes: ['read:groups', 'all:resources']
     }
   },
   '/group-roles/users/:userId': {
@@ -158,21 +158,21 @@ module.exports = {
       method: 'getGroupRoles',
       auth: 'jwt',
       access: [constants.UserRoles.Admin],
-      scopes: ['read:groups', 'write:groups', 'all:groups']
+      scopes: ['read:groups', 'write:groups', 'all:groups', 'all:resources']
     },
     post: {
       controller: 'GroupRoleController',
       method: 'addGroupRole',
       auth: 'jwt',
       access: [constants.UserRoles.Admin],
-      scopes: ['write:groups', 'all:groups']
+      scopes: ['write:groups', 'all:groups', 'all:resources']
     },
     delete: {
       controller: 'GroupRoleController',
       method: 'deleteGroupRole',
       auth: 'jwt',
       access: [constants.UserRoles.Admin],
-      scopes: ['write:groups', 'all:groups']
+      scopes: ['write:groups', 'all:groups', 'all:resources']
     }
   },
   '/health': {
