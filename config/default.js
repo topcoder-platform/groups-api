70e8fd6aefb1cbcfec1fb2ddfe76f87a25569935
bountyCoder <bountyc0d3r@gmail.com>
2023-02-10 10:49:33 +0000

    updated the config file for the cache TTL

--- config/default.js
+++ config/default.js
@@ -55,5 +55,5 @@ module.exports = {
   },
 
   REDIS_URL: process.env.REDIS_URL || 'redis://localhost:6379',
-  CACHE_TTL: process.env.CACHE_TTL || 3600
+  CACHE_TTL: process.env.CACHE_TTL ? Number(process.env.CACHE_TTL) : 3600
 }
